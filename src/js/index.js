c2b05648bec76a76de909dee5229d469cda6e036
VadimKol <47818256+VadimKol@users.noreply.github.com>
2019-06-21 05:44:50 +0300

    old changes plus buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 import "./import/modules.js";
 import "./import/components.js";
+
+$(document).ready(function(){
+    $('select').change(function(){
+        var width = $(this).width();
+        $(this).children(":disabled").remove();
+        $(this).width(width);
+    });
+});
